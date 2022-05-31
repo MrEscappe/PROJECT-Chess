ad767348d9cd14acbb8ef523ac389d3ea25417f0
José Sérgio <sergiosilva@outlook.com>
2022-05-31 19:40:06 -0300

    Creating board and a method to translate user input

---
+++ lib/board.rb
@@ -0,0 +1,30 @@
+# frozen_string_literal: true
+
+require_relative 'translate_input'
+
+class Board
+  attr_reader :board, :piece, :input
+
+  def initialize
+    @grid = Array.new(8) { Array.new(8) { '    ' } }
+    @input = TranslateInput.new
+    render_board(@grid)
+  end
+
+  def render_board(grid)
+    grid.reverse_each do |e|
+      puts '-----+------+------+------+------+------+------+-----+'
+      puts e.join(' | ')
+    end
+    puts '-----+------+------+------+------+------+------+-----+'
+    puts '  a      b      c      d      e      f      g      h'
+  end
+
+  def update_grid(row, column, piece)
+    @piece = piece
+    @grid[row][column] = piece
+    render_board(@grid)
+  end
+end
+
+board = Board.new
---
+++ lib/translate_input.rb
@@ -0,0 +1,36 @@
+# frozen_string_literal: true
+
+class TranslateInput
+  attr_reader :row, :column
+
+  def initialize
+    @row = nil
+    @column = nil
+  end
+
+  def translate(location)
+    position = location.split('')
+    letter = position[0]
+    number = (position[1..]).join.to_i
+    if (1..8).include?(number) && ('a'..'h').include?(letter)
+      translate_column(letter)
+      translate_row(number)
+    else
+      invalid_input
+    end
+  end
+
+  private
+
+  def translate_row(input)
+    @row = input.to_i - 1
+  end
+
+  def translate_column(input)
+    @column = input.downcase.ord - 97
+  end
+
+  def invalid_input
+    puts 'Please Input a valid position.'
+  end
+end
