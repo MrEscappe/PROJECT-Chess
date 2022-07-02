c89fb7f90894a5dc857fcb51171fff59ffa18821
José Sérgio <sergiosilva@outlook.com>
2022-07-01 21:44:30 -0300

    minor changes

--- main.rb
+++ main.rb
@@ -4,12 +4,4 @@ require_relative './lib/board'
 require_relative './lib/pieces/pieces'
 require_relative './lib/game'
 
-game = Game.new
-
-# test bishop
-
-# test all pawns on the board
-# ('a'..'h').each do |column|
-#     grid.move("#{column}2", "#{column}5")
-# end
-# grid.render
+game = Title.new
